47663aae752dd78376fdde62cba4d0297c85741d
Johann Boutet <johannboutet@gmail.com>
2015-07-28 09:00:11 -0400

    Fix bug with translations.

--- app/overrides/add_featured_switch_to_product_list.rb
+++ app/overrides/add_featured_switch_to_product_list.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => 'spree/admin/products/index',
 					 :name => 'add_header_column',
 					 :insert_before => "[data-hook='admin_products_index_header_actions']",
-					 :text => "<th><%= Spree.t(:'featured.col_title') %></th>")
+					 :text => "<th><%= Spree.t(:featured_col_title) %></th>")
 
 Deface::Override.new(:virtual_path => 'spree/admin/products/index',
 					 :name => 'add_switch_column',
